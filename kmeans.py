88d6ec4e9d75e33ffa2a69b4bb4954ebe765146a
Pipe <pipefutbol8@gmail.com>
2017-11-19 13:45:19 -0500

    actualizacion en kmeans

--- kmeans.py
+++ kmeans.py
@@ -1,16 +1,10 @@
-from numpy import array
-from math import sqrt
 from pyspark import SparkContext
 from random import randint
-from pyspark.sql import SparkSession
 from pyspark.mllib.clustering import KMeans, KMeansModel
 from pyspark.mllib.feature import HashingTF, IDF
 
-sc = SparkContext(appName="kmeans")  # SparkContext
-#spark = SparkSession.builder.appName("kmeans").getOrCreate()
-#sc = spark.sparkContext
+sc = SparkContext(appName="kmeans")
 
-#documents = sc.wholeTextFiles("/home/pipe/datasets/gutenberg/*.txt")
 docs = sc.wholeTextFiles("/home/pipe/datasets/gutenberg/*.txt")
 words = docs.values().map(lambda line: line.split(" "))
 
@@ -22,9 +16,6 @@ tfidf = idf.transform(tf)
 k = randint(3,6)
 
 clusters = KMeans.train(tfidf, k, maxIterations=10, initializationMode="random")
-
-#clusters.save(sc, "/home/pipe/Downloads/target/org/apache/spark/PythonKMeansExample/KMeansModel5")
-#sameModel = KMeansModel.load(sc, "/home/pipe/Downloads/target/org/apache/spark/PythonKMeansExample/KMeansModel5")
 
 print(clusters.predict(tfidf).collect())
 print("")
